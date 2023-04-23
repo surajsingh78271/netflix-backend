1c0edc8c276cb917bfddcef4ba19c1fab697eef8
surajsingh78271 <suraj10004@gmail.com>
2023-04-23 21:22:26 +0530

    db connection issue

--- db/models/signupmodel.js
+++ db/models/signupmodel.js
@@ -6,7 +6,10 @@ const userSchema = new mongoose.Schema({
     name: String,
     email:  String,
     password: {type: String}
-});
+},{strict:true,strictQuery:true});
+
+
+
 const userModel = new mongoose.model("usersignup",userSchema);
 
 
